b36818c6ffbdb7585b590c72d1f2dec5f8d81baa
Tommie Terhoeve <tommieterhoeve@gmail.com>
2019-09-13 18:38:32 +0200

    Created example of webapp

---
+++ Server/WebServer/WebApplication/WebApplication/Data/WebApplicationPagesMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.Models
+{
+    public class WebApplicationPagesMovieContext : DbContext
+    {
+        public WebApplicationPagesMovieContext (DbContextOptions<WebApplicationPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Server/WebServer/WebApplication/WebApplication/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication.Models;
+
+namespace WebApplication.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly WebApplication.Models.WebApplicationPagesMovieContext _context;
+
+        public CreateModel(WebApplication.Models.WebApplicationPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
