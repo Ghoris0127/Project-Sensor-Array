0cbeb090c564fd022ea9cdeb5a6a2091a41505a4
Tommie Terhoeve <tommieterhoeve@gmail.com>
2019-09-15 17:57:25 +0200

    Can show table of modules

---
+++ Server/WebServer/WebApplication/WebApplication/Module.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    /* Code for creating a module object when a module connects with the server */
+    public class Module
+    {
+        // Unique model ID
+        private int moduleID;
+        // Can contain any more detailed info on the module (expected location, purpose of module etc)
+        private string description;
+        // True when the module is connected, false when the module is disconnected
+        private bool isOnline;
+        // X,Y,Z coordinate
+        private int[] coordinates;
+        // place for x amount of unique sensors 
+        /* TODO (need a better way to make it a non-static value so that it can scale to the amount of unique sensors) */
+        private int[] lastSensorValues = new int[9];
+
+        // Constructor
+        public Module(int moduleID, string description, int[] coordinates, bool isOnline = false)
+        {
+            this.moduleID = moduleID;
+            this.description = description;
+            this.coordinates = coordinates;
+            this.isOnline = isOnline;
+        }
+
+
+        public int getModuleID()
+        {
+            return moduleID;
+        }
+
+        public string getDescription()
+        {
+            return description;
+        }
+        public int getCoordinates(int index)
+        {
+            return coordinates[index];
+        }
+        public bool getOnlineStatus()
+        {
+            return isOnline;
+        }
+    }
+}
--- Server/WebServer/WebApplication/WebApplication/Pages/Index.cshtml.cs
+++ Server/WebServer/WebApplication/WebApplication/Pages/Index.cshtml.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication;
 
 namespace WebApplication.Pages
 {
     public class IndexModel : PageModel
     {
-
-        public List<string> modulesOnline = new List<string> {"Module 1","Module 3" };
-        public List<string> moduelsOffline = new List<string> { "Module 2", "Module 4" };
-
+        public List<Module> modules = new List<Module> { new Module(0, "Dit is een test", new int[3] { 10, 10, 10 }), new Module(1, "Dit is een test", new int[3] { 30, 30, 30 }), new Module(2, "Dit is een test", new int[3] { 40, 10, 10 })};  
         public void OnGet()
         {
 
