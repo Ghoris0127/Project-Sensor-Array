b05c051b0ff2be018da6a9b972aa20b4300b794c
Tommie Terhoeve <tommieterhoeve@gmail.com>
2019-09-16 11:26:54 +0200

    charts not working

--- Server/WebServer/WebApplication/WebApplication/Pages/Index.cshtml.cs
+++ Server/WebServer/WebApplication/WebApplication/Pages/Index.cshtml.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using WebApplication;
-
+using System.Web.UI.DataVisualization.Charting;
 namespace WebApplication.Pages
 {
     public class IndexModel : PageModel
     {
-        public List<Module> modules = new List<Module> { new Module(0, "Dit is een test", new int[3] { 10, 10, 10 }), new Module(1, "Dit is een test", new int[3] { 30, 30, 30 }), new Module(2, "Dit is een test", new int[3] { 40, 10, 10 })};  
+        public List<Module> modules = new List<Module> { new Module(0, "Dit is een test", new int[3] { 10, 10, 10 }),
+                                                         new Module(1, "Dit is een test", new int[3] { 30, 30, 30 }),
+                                                         new Module(2, "Dit is een test", new int[3] { 40, 10, 10 })};  
         public void OnGet()
         {
 
